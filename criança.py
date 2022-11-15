3bbf71d5ff5fc7e18f8e4a1225ef82da6254d2b3
cauaa23 <116275269+cauaa23@users.noreply.github.com>
2022-11-14 21:35:15 -0300

    Create criança.py

---
+++ criança.py
@@ -0,0 +1,11 @@
+class Crianca:
+
+    def _init_(self, nome: str, idade: int):
+        self.nome = nome
+        self.idade = idade
+
+    def getNome(self):
+        return self.nome
+
+    def getIdade(self):
+        return self.idade
